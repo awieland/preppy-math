2dfe1f84e55853cbff5fef24108198087decdca2
Aaron Wieland <aaron@craftedapps.com>
2025-10-18 04:41:21 -0400

    Force latest version to be used by clients
    - immediately activate the new service worker
    - delete old caches

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,30 @@
-const CACHE_NAME = 'math-practice-v2';
+const CURRENT_CACHE_NAME = 'math-practice-v2';
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CURRENT_CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    }).then(() => self.skipWaiting()) // immediately activate the new SW
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          // Delete old caches that don't match the current version
+          if (cache !== CURRENT_CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // take control of all clients immediately
   );
 });
 
